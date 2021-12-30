4f76340bf0b944b8a43e5cf6719d0bc402a01052
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2021-12-30 07:58:57 -0600

    Chat-app

---
+++ src/utils/users.js
@@ -0,0 +1,47 @@
+const users = [];
+
+exports.addUser = ({ id, username, room }) => {
+  // Clean the data
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  // Validate
+  if (!username || !room) {
+    return {
+      error: "Username and room are required",
+    };
+  }
+
+  // Check for existing user
+  const existingUser = users.find(
+    (user) => user.room === room && user.username === username
+  );
+
+  // Validate username
+  if (existingUser) {
+    return {
+      error: "Username is in use",
+    };
+  }
+
+  // Store user
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
+};
+
+exports.removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+exports.getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+exports.getUsersInRoom = (room) => {
+  return users.filter((user) => user.room === room);
+};
